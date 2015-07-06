90f068afddc8905dfede08e872629618847e7e56
taove <taoveweb@gmail.com>
2015-07-06 15:59:09 +0800

    Update drag.js

--- drag.js
+++ drag.js
@@ -5,7 +5,7 @@ function drag(id){
   var obj=document.getElementById(id);
   var disX=0;
   var disY=0;
-  var str='这是一个测试中文的aaaaaaaa';
+  var str='这是一个测试中文的aaaasssssssssaaaa';
   obj.onmousedown=function(){
 
   }
